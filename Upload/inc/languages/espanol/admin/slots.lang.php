9e37ba5214dd5d6e655452b018adbb38bce3944f
vintagedaddyo <vintagedaddyo@gmail.com>
2021-03-13 23:24:28 -0800

    es

---
+++ Upload/inc/languages/espanol/admin/slots.lang.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Slots
+(c) 2007 by Jesse Labrocca
+Website: http://www.mybbcentral.com
+*/
+
+// plugin information
+
+$l['slots_name'] = 'Ranuras';
+
+$l['slots_description'] = 'Agrega una divertida máquina tragamonedas a su sitio usando NewPoints.';
+
+$l['slots_website'] = 'http://www.talkvideogames.com';
+
+$l['slots_author'] = 'Jesse Labrocca y ediciones de Vintagedaddyo';
+
+$l['slots_authorsite'] = 'http://www.mybbcentral.com';
+
+$l['slots_version'] = '1.2';
+
+$l['slots_compatibility'] = '18*';
+
+$l['slots_codename'] = '';
+
+// setting group
+
+$l['slots_settinggroup_title'] = 'Ranuras';
+
+$l['slots_settinggroup_description'] = 'Controla la configuración de las ranuras.';
+
+// setting 1
+
+$l['slots_setting_1_title'] = 'Crédito por giro';
+
+$l['slots_setting_1_description'] = 'Esto determina cuántos créditos de puntos nuevos se necesitan por tirada.';
+
+// link item
+
+$l['slots_link'] = 'Ranuras';
+
+// page
+
+$l['slots_page_1'] = 'Ranuras';
+
+$l['slots_page_2'] = 'Máquina tragamonedas Cryptx';
+
+$l['slots_page_3'] = 'Máquina tragamonedas xTreme';
+
+// you have
+
+$l['slots_youhave'] = 'Tú tienes';
+
+// you have
+
+$l['slots_spincost'] = 'Cada giro cuesta';
+
+// marquee
+
+$l['slots_marquee_no_win'] = '<span style="color: #0068AF;">AHAHAHA, buen intento.</span>';
+
+$l['slots_marquee_win'] = '<span style="color: #008000;">GANADOR - GANADOR - GANADOR </span>';
+
+$l['slots_marquee_spin'] = '<span style="color: #F50000;">Presione GIRAR para arriesgar su efectivo.</span>';
+
+// breadcrumb
+
+$l['slots_breadcrumb'] = 'Ranuras';
+
+$l['slots_breadcrumb_error'] = 'Error de ranuras';
+
+// error
+
+$l['slots_error_p1'] = 'Lo siento pero no tienes suficiente';
+
+$l['slots_error_p2'] = 'jugar.';
+
+?>
---
+++ Upload/inc/languages/espanol/slots.lang.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Slots
+(c) 2007 by Jesse Labrocca
+Website: http://www.mybbcentral.com
+*/
+
+// link item
+
+$l['slots_link'] = 'Ranuras';
+
+// page
+
+$l['slots_page_1'] = 'Ranuras';
+
+$l['slots_page_2'] = 'Máquina tragamonedas Cryptx';
+
+$l['slots_page_3'] = 'Máquina tragamonedas xTreme';
+
+// you have
+
+$l['slots_youhave'] = 'Tú tienes';
+
+// you have
+
+$l['slots_spincost'] = 'Cada giro cuesta';
+
+// marquee
+
+$l['slots_marquee_no_win'] = '<span style="color: #0068AF;">AHAHAHA, buen intento.</span>';
+
+$l['slots_marquee_win'] = '<span style="color: #008000;">GANADOR - GANADOR - GANADOR </span>';
+
+$l['slots_marquee_spin'] = '<span style="color: #F50000;">Presione GIRAR para arriesgar su efectivo.</span>';
+
+// breadcrumb
+
+$l['slots_breadcrumb'] = 'Ranuras';
+
+$l['slots_breadcrumb_error'] = 'Error de ranuras';
+
+// error
+
+$l['slots_error_p1'] = 'Lo siento pero no tienes suficiente';
+
+$l['slots_error_p2'] = 'jugar.';
+
+?>
