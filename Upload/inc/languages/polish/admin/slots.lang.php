d3dfc9a16fc70ba113a1c00d51d978df73a6d642
vintagedaddyo <vintagedaddyo@gmail.com>
2021-03-13 23:04:45 -0800

    pl

---
+++ Upload/inc/languages/polish/admin/slots.lang.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Slots
+(c) 2007 by Jesse Labrocca
+Website: http://www.mybbcentral.com
+*/
+
+// plugin information
+
+$l['slots_name'] = 'Sloty';
+
+$l['slots_description'] = 'Dodaje zabawny automat do Twojej witryny za pomocą NewPoints.';
+
+$l['slots_website'] = 'http://www.talkvideogames.com';
+
+$l['slots_author'] = 'Jesse Labrocca i edytuje Vintagedaddyo';
+
+$l['slots_authorsite'] = 'http://www.mybbcentral.com';
+
+$l['slots_version'] = '1.2';
+
+$l['slots_compatibility'] = '18*';
+
+$l['slots_codename'] = '';
+
+// setting group
+
+$l['slots_settinggroup_title'] = 'Sloty';
+
+$l['slots_settinggroup_description'] = 'Steruje ustawieniami gniazd.';
+
+// setting 1
+
+$l['slots_setting_1_title'] = 'Kredyt za obrót';
+
+$l['slots_setting_1_description'] = 'To określa, ile punktów Newpoints zajmuje na obrót.';
+
+// link item
+
+$l['slots_link'] = 'Sloty';
+
+// page
+
+$l['slots_page_1'] = 'Sloty';
+
+$l['slots_page_2'] = 'Automat do gier Cryptx';
+
+$l['slots_page_3'] = 'Automat xTreme';
+
+// you have
+
+$l['slots_youhave'] = 'Ty masz';
+
+// you have
+
+$l['slots_spincost'] = 'Każdy obrót kosztuje';
+
+// marquee
+
+$l['slots_marquee_no_win'] = '<span style="color: #0068AF;">AHAHAHA, dobra próba.</span>';
+
+$l['slots_marquee_win'] = '<span style="color: #008000;">ZWYCIĘZCA - ZWYCIĘZCA - ZWYCIĘZCA</span>';
+
+$l['slots_marquee_spin'] = '<span style="color: #F50000;">Naciśnij SPIN, aby zaryzykować swoją gotówkę.</span>';
+
+// breadcrumb
+
+$l['slots_breadcrumb'] = 'Sloty';
+
+$l['slots_breadcrumb_error'] = 'Błąd gniazd';
+
+// error
+
+$l['slots_error_p1'] = 'Przepraszam, ale nie masz dość';
+
+$l['slots_error_p2'] = 'grać.';
+
+?>
---
+++ Upload/inc/languages/polish/slots.lang.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Slots
+(c) 2007 by Jesse Labrocca
+Website: http://www.mybbcentral.com
+*/
+
+// link item
+
+$l['slots_link'] = 'Sloty';
+
+// page
+
+$l['slots_page_1'] = 'Sloty';
+
+$l['slots_page_2'] = 'Automat do gier Cryptx';
+
+$l['slots_page_3'] = 'Automat xTreme';
+
+// you have
+
+$l['slots_youhave'] = 'Ty masz';
+
+// you have
+
+$l['slots_spincost'] = 'Każdy obrót kosztuje';
+
+// marquee
+
+$l['slots_marquee_no_win'] = '<span style="color: #0068AF;">AHAHAHA, dobra próba.</span>';
+
+$l['slots_marquee_win'] = '<span style="color: #008000;">ZWYCIĘZCA - ZWYCIĘZCA - ZWYCIĘZCA</span>';
+
+$l['slots_marquee_spin'] = '<span style="color: #F50000;">Naciśnij SPIN, aby zaryzykować swoją gotówkę.</span>';
+
+// breadcrumb
+
+$l['slots_breadcrumb'] = 'Sloty';
+
+$l['slots_breadcrumb_error'] = 'Błąd gniazd';
+
+// error
+
+$l['slots_error_p1'] = 'Przepraszam, ale nie masz dość';
+
+$l['slots_error_p2'] = 'grać.';
+
+?>
